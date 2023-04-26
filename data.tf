47bf55c99f934e311b87c113d72881cf34a8fa35
Clifford Green <Greenclifford30@gmail.com>
2023-04-26 18:25:35 -0500

    Update availability zones

--- data.tf
+++ data.tf
@@ -5,3 +5,7 @@
 # data "aws_secretsmanager_secret_version" "aurora_creds" {
 #   secret_id = data.aws_secretsmanager_secret.aurora_creds.id
 # }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
