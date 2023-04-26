8eda032b4aaadc9bac91eccaab635ae47c623b06
Clifford Green <Greenclifford30@gmail.com>
2023-04-26 18:42:03 -0500

    update db

--- db.tf
+++ db.tf
@@ -1,9 +1,10 @@
 resource "aws_rds_cluster" "razorshop_db_cluster" {
-  engine             = "aurora"
+  engine             = "aurora-mysql"
+  engine_version = "5.7.mysql_aurora.2.03.2"
   engine_mode        = "serverless"
   database_name      = "razorshopdb"
   master_username    = jsondecode(data.aws_secretsmanager_secret_version.aurora_creds.secret_string)["username"]
-  master_password    = jsondecode(data.aws_secretsmanager_secret_version.aurora_creds.secret_string)["password"]
+  manage_master_user_password = true
   backup_retention_period = 7
   preferred_backup_window = "03:00-04:00"
   deletion_protection = false
@@ -24,15 +25,15 @@ resource "aws_rds_cluster" "razorshop_db_cluster" {
   }
 }
 
-resource "aws_rds_cluster_instance" "razorshop_db_instance" {
-  identifier         = "razorshop-db-instance"
-  engine             = "aurora"
-  instance_class     = "serverless"
-  apply_immediately  = true
-  cluster_identifier = aws_rds_cluster.razorshop_db_cluster.id
-  db_subnet_group_name = aws_db_subnet_group.aurora.name
+# resource "aws_rds_cluster_instance" "razorshop_db_instance" {
+#   identifier         = "razorshop-db-instance"
+#   engine             = "aurora"
+#   instance_class     = "serverless"
+#   apply_immediately  = true
+#   cluster_identifier = aws_rds_cluster.razorshop_db_cluster.id
+#   db_subnet_group_name = aws_db_subnet_group.aurora.name
 
-  tags = {
-    Name = "razorshop-db-instance"
-  }
-}
+#   tags = {
+#     Name = "razorshop-db-instance"
+#   }
+# }
