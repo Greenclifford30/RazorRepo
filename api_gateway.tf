b9c7bbe2941cb13074f12754ab5334a3bb508240
Clifford Green <Greenclifford30@gmail.com>
2023-05-14 13:13:44 -0500

    Status code change

--- api_gateway.tf
+++ api_gateway.tf
@@ -85,7 +85,7 @@ resource "aws_api_gateway_method_response" "services_mock_method_response_200" {
   rest_api_id = aws_api_gateway_rest_api.barbershop_api.id
   resource_id = aws_api_gateway_resource.services_resource.id
   http_method = aws_api_gateway_method.services_method.http_method
-  status_code = "200"
+  status_code = 200
 
   response_models = {
     "application/json" = "Empty"
